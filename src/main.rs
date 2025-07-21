5de20c76f13d1e8989a0ed1b30e1aba38d82223c
jam3sl1ng <jamesling596@gmail.com>
2025-07-21 21:02:49 +0100

    V0.1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .insert_resource(ClearColor(Color::srgb(155.0/255.0, 212.0/255.0, 195.0/255.0)))
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2d::default());
 }
