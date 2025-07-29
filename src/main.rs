d3e3cde73acd90ad3b17c65e7c404e83dbac6d53
jam3sl1ng <jamesling596@gmail.com>
2025-07-29 10:37:28 +0100

    V0.3

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@ use bevy::prelude::*;
 use bevy::window::PrimaryWindow;
 use bevy::window::WindowPlugin;
 
+const PLAYER_SPEED: f32 = 100.0;
+
+const CAMERA_DECAY_RATE: f32 = 1.0; // How quickly should the camera follow the player
+
 fn main() {
     App::new()
         .add_plugins(
@@ -21,8 +25,13 @@ fn main() {
         )
         .insert_resource(ClearColor(Color::srgb(155.0/255.0, 212.0/255.0, 195.0/255.0)))
         .add_systems(Startup, (setup_camera, spawn_player))
+        .add_systems(Update, (move_player, update_camera).chain())
         .run();
 }
+
+////////////////////////
+//////// CAMERA ////////
+////////////////////////
 
 fn setup_camera(
     mut commands: Commands,
@@ -41,6 +50,20 @@ fn setup_camera(
     } else {
         error!("Primary window not found!");
     }
+}
+
+pub fn update_camera(
+    mut camera: Single<&mut Transform, (With<Camera2d>, Without<Player>)>,
+    player: Single<&Transform, (With<Player>, Without<Camera2d>)>,
+    time: Res<Time>,
+) {
+    let Vec3 { x, y, .. } = player.translation;
+    let direction = Vec3::new(x, y, camera.translation.z);
+
+    // Smooth effect to move the camera position towards the player
+    camera
+        .translation
+        .smooth_nudge(&direction, CAMERA_DECAY_RATE, time.delta_secs());
 }
 
 ////////////////////////
@@ -78,4 +101,33 @@ pub fn spawn_player(
     } else {
         error!("Primary window not found!");
     }
+}
+
+// Update the player's position based on keyboard input
+pub fn move_player(
+    mut player: Single<&mut Transform, With<Player>>,
+    time: Res<Time>,
+    kb_input: Res<ButtonInput<KeyCode>>,
+) {
+    let mut direction = Vec2::ZERO;
+
+    if kb_input.pressed(KeyCode::KeyW) {
+        direction.y += 1.0;
+    }
+
+    if kb_input.pressed(KeyCode::KeyS) {
+        direction.y -= 1.0;
+    }
+
+    if kb_input.pressed(KeyCode::KeyA) {
+        direction.x -= 1.0;
+    }
+
+    if kb_input.pressed(KeyCode::KeyD) {
+        direction.x += 1.0;
+    }
+
+    // Continuously update the players position over time
+    let move_delta = direction.normalize_or_zero() * PLAYER_SPEED * time.delta_secs();
+    player.translation += move_delta.extend(0.0);
 }
